ceb15ee782bc28b4b86b3e91e7e2b5dafaf4a002
LuisVaniqueQuipux <luisvanique@gmail.com>
2025-09-03 19:05:57 -0300

    add: domains

--- src/main/java/br/com/luisvanique/provaTecnicaQuipux/domain/ListaDeReproducao.java
+++ src/main/java/br/com/luisvanique/provaTecnicaQuipux/domain/ListaDeReproducao.java
@@ -1,4 +1,44 @@
 package br.com.luisvanique.provaTecnicaQuipux.domain;
 
+import java.util.List;
+
 public class ListaDeReproducao {
+
+    private String nome;
+
+    private String descricao;
+
+    private List<Musica> musicas;
+
+    public ListaDeReproducao() {
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Musica> getMusicas() {
+        return musicas;
+    }
+
+    public void setMusicas(List<Musica> musicas) {
+        this.musicas = musicas;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+
+
+
 }
--- src/main/java/br/com/luisvanique/provaTecnicaQuipux/domain/Musica.java
+++ src/main/java/br/com/luisvanique/provaTecnicaQuipux/domain/Musica.java
@@ -1,4 +1,15 @@
 package br.com.luisvanique.provaTecnicaQuipux.domain;
 
+import java.time.LocalDate;
+
 public class Musica {
+    private String titulo;
+
+    private String artista;
+
+    private String album;
+
+    private LocalDate ano;
+
+    private String genero;
 }
