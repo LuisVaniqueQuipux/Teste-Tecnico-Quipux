9c716904230b7bba9e70abfc14d8a27e21e3ea26
LuisVanique <cunhagustavo142@gmail.com>
2025-09-03 18:40:57 -0300

    first commit

---
+++ src/main/java/br/com/luisvanique/provaTecnicaQuipux/ProvaTecnicaQuipuxApplication.java
@@ -0,0 +1,13 @@
+package br.com.luisvanique.provaTecnicaQuipux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaTecnicaQuipuxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaTecnicaQuipuxApplication.class, args);
+	}
+
+}
